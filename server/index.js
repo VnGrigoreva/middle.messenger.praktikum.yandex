c131d8e70d39b702107f4fff768b10ea222ee213
VnGrigoreva <vn.grigoreva@yandex.ru>
2022-01-12 02:32:17 +0300

    add server

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 4000;
+
+app.use(express.static('./'));
+
+app.listen(port, function () {
+  console.log(`http://localhost:${port}/ server is on`);
+});
+
+app.use(express.static(path.resolve(__dirname,"../dist")));
+
+app.get("/", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "../dist/index.html"));
+});
