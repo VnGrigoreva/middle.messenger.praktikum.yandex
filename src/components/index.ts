69db00467ae4da7e4f34589e690dd159bdcdd196
VnGrigoreva <vn.grigoreva@yandex.ru>
2022-02-20 14:47:12 +0300

    fixes

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,4 @@
 export {Block} from './Block/Block';
 export {Input} from './Input/Input';
 export {Link} from './Link/Link';
-export {Error400Page} from '../pages/errorPage/Error400Page/Error400Page';
+export {Error400Page} from '../pages/errorPages/Error400Page/Error400Page';
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -4,5 +4,5 @@ export {Profile} from './profile/Profile/Profile';
 export {Settings} from './profile/Settings/Settings';
 export {PasswordEditing} from './profile/PasswordEditing/PasswordEditing';
 export {Chat} from './chats/Chat';
-export {Error400Page} from './errorPage/Error400Page/Error400Page';
-export {Error500Page} from './errorPage/Error500Page/Error500Page';
+export {Error400Page} from './errorPages/Error400Page/Error400Page';
+export {Error500Page} from './errorPages/Error500Page/Error500Page';
